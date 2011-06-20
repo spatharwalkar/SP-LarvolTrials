65bafb19cbbbaa48a159c13d0e8899beb0ab5fbf
Anthony Heathcoat <anthony.heathcoat@larvol.com>
2011-06-20 20:04:19 +0000

    added start date to nct full refresh

--- fetch_nct_fullhistory_all.php
+++ fetch_nct_fullhistory_all.php
@@ -9,11 +9,15 @@ $query = 'SELECT MAX(val_int) AS maxid FROM data_values WHERE `field`=' . $fid;
 $res = mysql_query($query) or die('Bad SQL query finding highest nct_id');
 $res = mysql_fetch_array($res) or die('No nct_id found!');
 $maxid = $res['maxid'];
-echo('Refreshing up to: ' . $maxid . '<br />'); @flush();
-for($cid = 1; $cid <= $maxid; ++$cid)
+//find start id
+$cid = (isset($_GET['start']) && is_numeric($_GET['start'])) ? ((int)$_GET['start']) : 1;
+//go
+echo('Refreshing from: ' . $cid . ' to: ' . $maxid . '<br />'); @flush();
+for(; $cid <= $maxid; ++$cid)
 {
 	$_GET['id'] = $cid;
 	require('fetch_nct_fullhistory.php');
+	@flush();
 }
 echo('Done with all IDs.');
 ?>
