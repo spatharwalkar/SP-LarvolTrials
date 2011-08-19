d00b5e60eb1826e3641fc0df1215c0cc42c83ff2
Jithu Thomas <jithu.thomas@larvol.com>
2011-08-19 05:50:28 +0000

    Refs #46
    moved values from settings_template.php to settings_template.ini  and added ini parsing code in settings_template.php

--- settings_template.php
+++ settings_template.php
@@ -1,30 +1,9 @@
 <?php
-/* Database login settings.
-	You must change these to point to the correct database.
-*/
-define('DB_SERVER', '127.0.0.1');
-define('DB_NAME', 'clinicaltrials');
-define('DB_USER', 'clinical_user');
-define('DB_PASS', 'password1234');
-//Name of secondary database for temporary usage
-define('DB_TEMP', 'LT_temp');
-
-//Enable or disable mail. If disabled, mail contents will be written to a text file.
-define('MAIL_ENABLED', true);
-define('MAX_EMAIL_FILES', 10);  // maximum no. of email files to keep (including attachments)
-
-/* YOURLS options.
-	You must change these to reflect how you set up YOURLS
-*/
-define('YOURLS_USER', 'root');
-define('YOURLS_PASS', 'password');
-define('YOURLS_URL', 'http://localhost/s/yourls-api.php');
-
-/* Miscellaneous site options.
-	Customize these however you wish.
-*/
-define('SITE_NAME','Larvol Trials');	//name used by the site to refer to itself in communiction with users
-
+$settingsArray = parse_ini_file('setup/settings.ini');
+foreach($settingsArray as $settings=>$value)
+{
+	define($settings,$value);
+}
 //Don't change these
 define('HASH_ALGO', 'tiger192,4');
 ini_set('magic_quotes_gpc','Off');
