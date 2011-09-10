0923188ff4958553ef18483d62a9d5e578537ca6
Jithu Thomas <jithu.thomas@larvol.com>
2011-09-10 08:57:02 +0000

    Refs #12 Latest code tested in the test server.

--- entry.php
+++ entry.php
@@ -8,6 +8,7 @@ if(!$db->loggedIn())
 	header('Location: ' . urlPath() . 'index.php');
 	exit;
 }
+ini_set('max_execution_time','360000');	//100 hours
 //add new record on submitting the add new record button
 if($_POST['add_new_record'])
 {
@@ -46,26 +47,42 @@ foreach($db->sources as $source)
 }
 
 $sourceIds = implode(',',$sourceIds);
-// old query. $query = "SELECT clinical_study.larvol_id FROM clinical_study WHERE larvol_id NOT IN(SELECT DISTINCT data_cats_in_study.larvol_id FROM data_values LEFT JOIN data_cats_in_study ON data_values.studycat=data_cats_in_study.id WHERE `field` IN(".$sourceIds."))";
-$query = 	"SELECT DISTINCT clinical_study.larvol_id AS larvol_id
+// old query. 
+//echo $query = "SELECT clinical_study.larvol_id FROM clinical_study WHERE larvol_id NOT IN(SELECT DISTINCT data_cats_in_study.larvol_id FROM data_values LEFT JOIN data_cats_in_study ON data_values.studycat=data_cats_in_study.id WHERE `field` IN(".$sourceIds."))";
+//die;
+/*$query = 	"SELECT DISTINCT clinical_study.larvol_id AS larvol_id
 			from clinical_study
 			LEFT JOIN 
 			(
 			SELECT DISTINCT data_cats_in_study.larvol_id
 			FROM data_values
 			LEFT JOIN data_cats_in_study ON data_values.studycat = data_cats_in_study.id
-			WHERE `field`
-			IN (".$sourceIds.") 
+			WHERE `field` = 1
+			
 			) AS res
 			ON res.larvol_id=clinical_study.larvol_id
 			WHERE
-			res.larvol_id IS NULL";
+			res.larvol_id IS NULL";*/
 
-
-$res = mysql_query($query) or die('Bad SQL query getting no source field larvol_ids');
-while($rw = mysql_fetch_assoc($res))
+$query1 = "SELECT DISTINCT data_cats_in_study.larvol_id FROM data_values , data_cats_in_study where data_values.studycat=data_cats_in_study.id and `data_values`.`field` IN(".$sourceIds.")";
+$query2 = "select larvol_id from clinical_study";
+$result1 = mysql_query($query1);
+while($row1 = mysql_fetch_assoc($result1))
 {
-	echo '<br/><a alt="edit" href="inspect.php?larvol_id='.$rw['larvol_id'].'&inspect=Lookup"><img src="images/jedit.png"/>'.$rw['larvol_id'].'</a>';
+	$sourceLarvolIds[] = $row1['larvol_id'];
+}
+$result2 = mysql_query($query2);
+$entryArr = array();
+ob_implicit_flush(true);
+while($row2 = mysql_fetch_assoc($result2))
+{
+
+	if(!in_array($row2['larvol_id'],$sourceLarvolIds))
+	{
+		echo '<br/><a alt="edit" target="_blank" href="inspect.php?larvol_id='.$row2['larvol_id'].'&inspect=Lookup"><img src="images/jedit.png"/>'.$row2['larvol_id'].'</a>';
+
+	}
+	
 }
 ?>
 </div>
