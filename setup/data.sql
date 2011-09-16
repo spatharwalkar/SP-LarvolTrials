7d4b92548cfa299a6686c82e70b11e603f134314
Jithu Thomas <jithu.thomas@larvol.com>
2011-09-16 18:34:23 +0000

    Refs #34 Correction in data.sql removed duplicate entry

--- setup/data.sql
+++ setup/data.sql
@@ -372,7 +372,6 @@ INSERT INTO `data_fields` (`id`, `name`, `type`, `category`) VALUES
 (515, 'keyword', 'varchar', 1),
 (516, 'study_id_org_name','varchar',1),
 (517, 'study_id_org_full_name','varchar',1),
-(518, 'org_study_id','varchar',1),
 (519, 'is_fda_regulated','bool',1),
 (520, 'is_section_801','bool',1),
 (521, 'delayed_posting','bool',1),
