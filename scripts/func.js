c05bd78733c07cd6ea86627bffefed6631048f41
Anthony Heathcoat <anthony.heathcoat@larvol.com>
2011-09-28 21:23:58 +0000

    Refs #19 Changed upm button to say "Milestones" instead of "UPMs" and added hand pointer css.

--- scripts/func.js
+++ scripts/func.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
 	$('.upms').hide();
-	$('#addtoright').after('&nbsp;&nbsp;&nbsp;<span id="addedtoright" onclick="sh(this,0,1);">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;All Unmatched UPMs</span>');
+	$('#addtoright').after('&nbsp;&nbsp;&nbsp;<span id="addedtoright" onclick="sh(this,0,1);">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;All Milestones</span>');
 	$('#addedtoright').css('background-image','url(\'./images/up.png\')')
 	.css('background-repeat','no-repeat')
 	.css('background-position','left center')
 	.css('border','1px solid')
 	.css('padding','2px')
+	.css('cursor','pointer')
 	.css('margin-left','200px');
 });
 
