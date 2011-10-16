147c11fb7913b06de3273558219d3f47d69f128d
Vinod TK <vinod.tk@larvol.com>
2011-10-16 15:34:15 +0000

    Refs #32 Added new irregular phase value "I-II" into the function validateEnums Tweaked the function for fixing Unicode characters.

--- special_chars.php
+++ special_chars.php
@@ -19,8 +19,8 @@ function fix_special_chars($in_txt)
 	$in_txt = str_replace('“','"', $in_txt);
 	$in_txt = str_replace('”','"', $in_txt);
 	$in_txt = str_replace('`',",", $in_txt);
-	$in_txt = str_replace('¼',"&#188;", $in_txt);
-	$in_txt = str_replace('½',"&#189;", $in_txt);
+//	$in_txt = str_replace('¼',"&#188;", $in_txt);
+//	$in_txt = str_replace('½',"&#189;", $in_txt);
 	
 	$in_txt = iconv("UTF-8","UTF-8//IGNORE",$in_txt); 
 	
