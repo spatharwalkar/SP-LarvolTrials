d540f819ff9d8b30ee8c2b0c01ec4ce8442acd0b
Vinod TK <vinod.tk@larvol.com>
2012-01-26 15:51:23 +0000

    Output buffering related commands removed from index_area.php and index_product.php as requested by Jithu.

--- index_area.php
+++ index_area.php
@@ -2,8 +2,6 @@
 require_once('db.php');
 require_once('preindex_trial_new.php');
 ini_set('max_execution_time', '36000'); //10 hours
-ob_implicit_flush(true);
-ob_end_flush();
 
 if (isset($_GET['id'])) 
 {
