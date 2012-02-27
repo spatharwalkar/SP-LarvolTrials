c5e5f9ab99af880e71a89927562e30d56ad96967
Priyadarshini Nadar <priyadarshini.nadar@larvol.com>
2012-02-27 19:58:42 +0000

    Refs #76 removed extra curly brace

--- intermediary.php
+++ intermediary.php
@@ -61,7 +61,6 @@ $sortFields = array('phase' => 'pD', 'inactive_date' => 'iA', 'start_date' => 's
 $sortFieldName = array('phase' => 'Phase', 'inactive_date' => 'End Date', 'start_date' => 'Start Date', 'overall_status' => 'Status', 'enrollment' => 'N');
 
 $lastChangedTime = filectime("css/intermediary.css");
-}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
