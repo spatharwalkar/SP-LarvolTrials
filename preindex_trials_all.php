eeb311b07eba699a445d92e436d6b37da4f24032
Vinod TK <vinod.tk@larvol.com>
2012-03-16 18:38:42 +0000

    Will ignore user abort

--- preindex_trials_all.php
+++ preindex_trials_all.php
@@ -2,7 +2,7 @@
 require_once('db.php');
 require_once('preindex_trial.php');
 ini_set('max_execution_time', '360000'); //100 hours
-
+ignore_user_abort(true);
 //index all products
 echo '<br><b>Indexing ALL products...<br></b>';
 tindex(NULL,'products',NULL,NULL,NULL,NULL);
