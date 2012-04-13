f3d216b1566a7d9cd7d374e0c0863fb052a74846
Anthony Heathcoat <anthony.heathcoat@larvol.com>
2012-04-13 20:32:04 +0000

    Refs #92 Refs #93 New fields for master hm cells

--- setup/schema.sql
+++ setup/schema.sql
@@ -872,11 +872,15 @@ CREATE TABLE IF NOT EXISTS `rpt_masterhm_cells` (
   `bomb_auto` enum('none','small','large') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'none' COMMENT 'Indication that LT suggests a bomb for this cell',
   `bomb_explain` text COLLATE utf8_unicode_ci COMMENT 'Analysts'' explanation of bomb',
   `highest_phase` enum('N/A','0','0/1','1','1a','1b','1a/1b','1c','1/2','1b/2','1b/2a','2','2a','2a/2b','2b','2/3','2b/3','3','3a','3b','3/4','3b/4','4') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'N/A',
+  `highest_phase_prev` enum('N/A','0','0/1','1','1a','1b','1a/1b','1c','1/2','1b/2','1b/2a','2','2a','2a/2b','2b','2/3','2b/3','3','3a','3b','3/4','3b/4','4') COLLATE utf8_unicode_ci DEFAULT NULL,
+  `highest_phase_lastchanged` datetime DEFAULT NULL,
   `phase4_override` tinyint(1) NOT NULL DEFAULT '0',
   `phase_explain` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `phase_explain_lastchanged` datetime DEFAULT NULL,
   `filing` text COLLATE utf8_unicode_ci DEFAULT NULL,
   `filing_lastchanged` datetime DEFAULT NULL,
   `last_update` datetime DEFAULT NULL,
+  `last_calc` datetime DEFAULT NULL,
   PRIMARY KEY (`product`,`area`),
   KEY `area` (`area`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
