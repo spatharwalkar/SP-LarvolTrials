342c6623d418eda665605e1c493926530760f999
Jithu Thomas <jithu.thomas@larvol.com>
2012-08-01 18:41:36 +0000

    [#33141269] @individual product sync fetch_li_product_individual(liProductId)
    @full refresh script now using the individual product sync.
    
    @additional get paramter fetch_li_script_product_individual_id that can be used to run from fetch_li_products.php to sync individual product.
    
    @vinod invoke the function instead of the new get parameter for proper usage.
    
    Thanks

--- fetch_li_products.php
+++ fetch_li_products.php
@@ -1,7 +1,6 @@
 <?php
 require_once 'db.php';
 require_once 'include.page.php';
-
 ini_set('max_execution_time','360000');	//100 hours
 
 /**
@@ -26,14 +25,33 @@ function fetch_li_products($lastrun)
 	foreach($xmlImportProductList->getElementsByTagName('Product_ID') as $Product_ID)
 	{
 		$Product_ID = $Product_ID->nodeValue;
-		$liXmlProduct = file_get_contents('http://admin.larvolinsight.com/LT/Services/Products/Detail.ashx?product_id='.$Product_ID);
-		$xmlImportProduct = new DOMDocument();
-		$xmlImportProduct->loadXML($liXmlProduct);
-		$out = parseProductsXmlAndSave($xmlImportProduct,'products');
-		echo 'Imported '.$out['success'].' records, Failed entries '.$out['fail']."<br/>\n";
+		fetch_li_product_individual($Product_ID);
 	}
 }
+/**
+ * @name fetch_li_product_individual
+ * @tutorial Function to sync individual LI products with LT products. All data are retrieved from LI in xml format.
+ * @param String $Product_ID = products.LI_id in LT 
+ * @author Jithu Thomas
+ */
+function fetch_li_product_individual($Product_ID)
+{
+	$liXmlProduct = file_get_contents('http://admin.larvolinsight.com/LT/Services/Products/Detail.ashx?product_id='.$Product_ID);
+	$xmlImportProduct = new DOMDocument();
+	$xmlImportProduct->loadXML($liXmlProduct);
+	$out = parseProductsXmlAndSave($xmlImportProduct,'products');
+	echo 'Imported '.$out['success'].' records, Failed entries '.$out['fail']."<br/>\n";	
+}
+
+//controller
 $timeStamp = '';
 $timeStamp = $_GET['li_product_sync_timestamp'];
 if($timeStamp !=='' && is_numeric($timeStamp))
-fetch_li_products($timeStamp);
+{
+	fetch_li_products($timeStamp);
+}
+$fetchLiScriptProductIndividualId = $_GET['fetch_li_script_product_individual_id'];
+if($fetchLiScriptProductIndividualId !='')
+{
+	fetch_li_product_individual($fetchLiScriptProductIndividualId);
+}
