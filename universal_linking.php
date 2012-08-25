4c75cc8838241afef15bfae8e4a3c49db6ce08ec
Vinod TK <vinod.tk@larvol.com>
2012-08-25 08:09:37 +0000

    [#34965039] Fix for Maximum execution time limit.

--- universal_linking.php
+++ universal_linking.php
@@ -1,6 +1,7 @@
 <?
 //ini_set('error_reporting', E_ALL );
 ini_set('memory_limit','256M');
+ini_set('max_execution_time','36000');	//10 hours
 require_once('db.php');
 require_once('include.util.php');
 require_once('header.php');
