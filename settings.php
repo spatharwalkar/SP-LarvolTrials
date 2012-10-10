1175b052eefd8d7e6d8ff435e92a4cc86b66c1b3
Anthony Heathcoat <anthony.heathcoat@larvol.com>
2012-10-10 17:26:44 +0000

    added header X-UA-Compatible sent via php in settings loader

--- settings.php
+++ settings.php
@@ -44,4 +44,5 @@ ini_set('magic_quotes_gpc','Off');
 ini_set('magic_quotes_runtime','Off');
 ini_set('magic_quotes_sybase','Off');
 ini_set('register_globals','0');
+header("X-UA-Compatible: IE=Edge");
 ?>
