2bc48275942099e7dd93bbf8b5146ba97252007f
Vinod TK <vinod.tk@larvol.com>
2012-11-04 08:13:07 +0000

    fix for the login cookie issue from LI (now the cookie can be accessed from both www.larvoltrials.com and http://larvoltrials.com)

--- api/b.php
+++ api/b.php
@@ -1,5 +1,5 @@
 <?php
-setcookie('li_user',$_GET['u'],time()+(3600*24*365*31),'/'); //31 years
+setcookie('li_user',$_GET['u'],time()+(3600*24*365*31),'/','.larvoltrials.com'); //31 years
 header('Content-Type: image/gif');
 readfile('../images/beacon.gif');
 ?>
