bdf4524b56377aa1ff4e5fe08089f364a5b10e64
Vinod TK <vinod.tk@larvol.com>
2013-05-12 10:54:20 +0000

    Added more checks to cell calculation to avoid false changes.

--- calculate_hm_cells.php
+++ calculate_hm_cells.php
@@ -746,7 +746,14 @@ function add_data($entity1id,$entity2id,$cnt_total,$cnt_active,$cnt_active_indle
 			if($count_total_old<>$cnt_total) $bb='`count_total_prev` = "'. $count_total_old .'",';
 			if($cnt_indlead_old<>$cnt_active_indlead) $cc='`count_active_indlead_prev` = "'. $cnt_indlead_old .'",';
 			if($cnt_owner_sponsored_old<>$cnt_active_owner_sponsored) $ee='`count_active_owner_sponsored_prev` = "'. $cnt_owner_sponsored_old .'",';
-			if($highest_phase_old<>$max_phase) $dd='`highest_phase_prev` = "'. $highest_phase_old .'",';
+			if( empty($highest_phase_old) or empty($max_phase) or $max_phase <= $highest_phase_old)
+			{
+				if($cnt_total==0) $dd='`highest_phase_prev` = null,';
+			}
+			else
+			{
+				if($highest_phase_old<>$max_phase) $dd='`highest_phase_prev` = "'. $highest_phase_old .'",';
+			}
 			if($cnt_total==0) $dd='`highest_phase_prev` = null,';
 		}
 		
