dd2c9da58a2e69d463a31628f4508cd67ebde7fe
Somil Kulshreshtha <somil.kulshreshtha@larvol.com>
2013-07-03 04:44:47 +0000

    [#48360189] Initial trail page. Now this will have only the source links + data table shown

--- trial.php
+++ trial.php
@@ -5,13 +5,11 @@ require_once('db.php');
 
 //ini_set('error_reporting', E_ALL ^ E_NOTICE);
 global $logger;
-$table = 'products';
-$table1 = 'data_trials';
-$script = 'edit_trials';
+
 
 // The table is not displayed properly in Chrome, but works fine in MSIE and FireFox.  Something to do with Doctype
 // So a hack is used to fix the issue. 
-if(stripos($_SERVER['HTTP_USER_AGENT'],'chrome')) echo '<!DOCTYPE>';
+//if(stripos($_SERVER['HTTP_USER_AGENT'],'chrome')) echo '<!DOCTYPE>';
 		
 require_once('header.php');	
 global $db;
@@ -146,15 +144,15 @@ $query = 	"
 		$fields[$col] = ucfirst(str_replace( "_", " ", $col));
 	}
 	
+	foreach($fields as $field => $field_name)
+	{
+		$mapping_eudract[$field] 	= $field;
+		$mapping_nct[$field] 		= $field;
+	}
+	$mapping_eudract['source_id'] = 'eudract_id';
+	$mapping_nct['source_id'] 		= 'nct_id';
+	
 ?>
-<script type="text/javascript" src="scripts/jquery-1.7.1.min.js"></script>
-<script type="text/javascript" src="scripts/jquery-ui-1.8.17.custom.min.js"></script>
-<link href="css/themes/cupertino/jquery-ui-1.8.17.custom.css" rel="stylesheet" type="text/css" media="screen" />
- <script>
-$(function() {
-$( "#trial_tabs" ).tabs();
-});
-</script>
  <style>
  body
 {
@@ -168,141 +166,74 @@ a:visited {color:#6600bc;}  /* visited link */
 /*a:hover {color:#FF00FF;}  /* mouse over link */
 /*a:active {color:#0000FF;}  /* selected link */
 
-.ReportHeading
+table#table-trial
 {
-	color: rgb(83, 55, 130);
-	font-size: xx-large;
-	font-weight: bold;
+width:100%;
+table-layout:fixed;
 }
-
-.SearchBttn1
+#container-trial #table-trial th,
+#container-trial #table-trial td
 {
-	width:100px;
-	height:35px;
-	background-color:#4f2683;
-	font-weight:bold;
-	color:#FFFFFF;
+width: 15%;
+word-wrap: break-word;
+height: 50px;
 }
-
-.FoundResultsTb
-{
-	background-color:#aa8ece;
-	border:0;
-	border-top:#4f2683 solid 2px;
-}
-
-#FoundResultsTb a {
-display:inline;
-}
-.selectTab
-{
-	background-image:url(images/selectTab.png); 
-	background-repeat:repeat-x;
-}
-
-.Tab
-{
-	background-image:url(images/Tab.png); 
-	background-repeat:repeat-x;
-}
-
-#disease_tabs a
-{
-	text-decoration:none;
-	color:#000000;
-	font-size:13px;
-	font-family:Arial, Helvetica, sans-serif;
-	display:block;
-}
-
-#diseaseTab_content
-{
-    background-color: #ffffff;
-    padding: 30px;
-	border-top:#d333333 solid 1px;
-}
-
-.list_fileds{list-style-type:none; padding-bottom: 20px;}
-label{font-weight:bold; margin-bottom: -6px;background: none;}
-.ui-widget-content{background: none;}
-.ui-widget{font-family:Arial;}
 </style>
 <div id="container-trial"  style="margin:8px">
 
-<?php  
-//echo '<pre>';
-//print_r($opts);
-//print_r($hint);
-?>	
 <br />
 
-		
-<div id="trial_tabs" class="trial_tabs" style="">
-	<ul>
-		<li><a href="#tab-manual">Manual</a></li>
-		<li><a href="#tab-eudract">EudraCT</a></li>
-		<li><a href="#tab-nct">NCT</a></li>
-		<li><a href="#tab-trial">Current Value</a></li>
-		<li><a href="#tab-prev">Previous Value</a></li>
-		<li><a href="#tab-lastchangedon">Last changed on</a></li>
-	</ul>
+<div id="tab-eudract">
+Source Eudract:
+<?php 
+if($eudracts['eudract_id'] != ''){
+	$ctLink = 'https://www.clinicaltrialsregister.eu/ctr-search/search?query=' . padeudra($eudracts['eudract_id']);
+	echo "<a href='".$ctLink."'>$ctLink</a>";
+}else{
+	echo 'None';
+}
 
-	<div id="tab-manual">
-	<?php foreach($fields as $field_key => $field_value){ ?>
-		<li class="list_fileds" id="manual_<?php echo $field_key;?>">
-			<label><?php echo $field_value?> : </label>
-					<?php echo (isset($manuals[$field_key]) ? $manuals[$field_key] : '');?>
-		</li>
-	<?php } ?>
-	</div>
+?>
+</div>
 
-	<div id="tab-eudract">
-	<?php foreach($fields as $field_key => $field_value){ ?>
-		<li class="list_fileds" id="eudract_<?php echo $field_key;?>">
-			<label><?php echo $field_value?> : </label>
-					<?php echo (isset($eudracts[$field_key]) ? $eudracts[$field_key] : '');?>
-		</li>
-	<?php } ?>
-	</div>
-	
-	<div id="tab-nct">
-	<?php foreach($fields as $field_key => $field_value){ ?>
-		<li class="list_fileds" id="nct_<?php echo $field_key;?>">
-			<label><?php echo $field_value?> : </label>
-					<span><?php echo (isset($ncts[$field_key]) ? $ncts[$field_key] : '');?></span>
-		</li>
-	<?php } ?>
-	</div>
+<div id="tab-nct">
+Source Nct:
+<?php 
+if($ncts['nct_id'] != ''){
+	$ctLink = 'http://clinicaltrials.gov/ct2/show/' . padnct($ncts['nct_id']);
+	echo "<a href='".$ctLink."'>$ctLink</a>";
+}
 
-	<div id="tab-trial">
-	<?php foreach($fields as $field_key => $field_value){ ?>
-		<li class="list_fileds" id="trial_<?php echo $field_key;?>">
-			<label><?php echo $field_value?> : </label>
-					<?php echo (isset($trials[$field_key]) ? $trials[$field_key] : '');?>
-		</li>
-	<?php } ?>
-	</div>
-
-	<div id="tab-prev">
-	<?php foreach($fields as $field_key => $field_value){ ?>
-		<li class="list_fileds" id="prev_<?php echo $field_key;?>">
-			<label><?php echo $field_value?> : </label>
-					<?php echo (isset($history[$field_key.'_prev']) ? $history[$field_key.'_prev'] : '');?>
-		</li>
-	<?php } ?>
-	</div>
-	
-	<div id="tab-lastchangedon">
-	<?php foreach($fields as $field_key => $field_value){ ?>
-		<li class="list_fileds" id="lastchangedon_<?php echo $field_key;?>">
-			<label><?php echo $field_value?> : </label>
-					<?php echo (isset($history[$field_key.'_lastchanged']) ? $history[$field_key.'_lastchanged'] : '');?>
-		</li>
-	<?php } ?>
-	</div>
-	
+?>
 </div>
 
 
+<p>&nbsp;</p>
+
+<table cellspacing="0" cellpadding="5" id="table-trial">
+        <thead>
+            <tr>
+            <th>Fields</th>
+            <th>Manual</th>
+            <th>Eudract</th>
+            <th>NCT</th>
+            <th>Current Value</th>
+            <th>Previous Value</th>
+            <th>Last changed on</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php $i = 0; foreach($fields as $field_key => $field_name){ $i++;?>
+            <tr><th><?php echo $i.'. '.$field_name?> </th>
+            <td><?php echo (isset($manuals[$field_key]) ? $manuals[$field_key] : '');?></td>
+            <td><?php echo (isset($eudracts[$mapping_eudract[$field_key]] ) ? $eudracts[$mapping_eudract[$field_key]] : '');?></td>
+            <td><?php echo (isset($ncts[$mapping_nct[$field_key]]) ? $ncts[$mapping_nct[$field_key]] : '');?></td>
+            <td><?php echo (isset($trials[$field_key]) ? $trials[$field_key] : '');?></td>
+            <td><?php echo (isset($history[$field_key.'_prev']) ? $history[$field_key.'_prev'] : '');?></td>
+            <td><?php echo (isset($history[$field_key.'_lastchanged']) ? $history[$field_key.'_lastchanged'] : '');?></td></tr>
+		<?php } ?>
+        </tbody>
+</table>
+    
 
 </div>
