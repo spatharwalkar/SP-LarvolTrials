f73ff8029f689cad9819c9a487b943b6d92110be
Vinod TK <vinod.tk@larvol.com>
2013-07-24 17:32:27 +0000

    [#44828043] Event_type hidden in UPM editing page - by Daljit

--- upm.php
+++ upm.php
@@ -389,7 +389,7 @@ if($_REQUEST['add_new_record']=='Add New Record' || $_REQUEST['id'] || $saveStat
 	$addEditGlobalInputStyle = 'style="width:97%;min-width:200px;"';
 	$id = ($_GET['id'])?$_GET['id']:null;
 	echo '<div>';
-	addEditUpm($id,$table,$script,array("formOnSubmit"=>"onsubmit=\"return validateedit();\"",'deletebox'=>false,'saveStatus'=>$saveStatus,'formStyle'=>$addEditFormStyle,'mainTableStyle'=>$mainTableStyle,'addEditGlobalInputStyle'=>$addEditGlobalInputStyle),array('last_update'));
+	addEditUpm($id,$table,$script,array("formOnSubmit"=>"onsubmit=\"return validateedit();\"",'deletebox'=>false,'saveStatus'=>$saveStatus,'formStyle'=>$addEditFormStyle,'mainTableStyle'=>$mainTableStyle,'addEditGlobalInputStyle'=>$addEditGlobalInputStyle),array('last_update', 'event_type'));
 	echo '</div>';
 }
 
