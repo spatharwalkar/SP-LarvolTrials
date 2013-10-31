9c4fa80c641aea79844efbd18b000c438f2dcc6f
Anthony Heathcoat <anthony.heathcoat@larvol.com>
2013-10-31 18:16:16 +0000

    [#59779682] Fixed linkedin integration - use email as unique identifier since the actual "identifier" given by linkedin seems to change per session

--- sigma/login.php
+++ sigma/login.php
@@ -20,29 +20,48 @@ if(!$db->loggedIn())
 	{
 		$hybridauth = new Hybrid_Auth( $config );
 		$linkedin = $hybridauth->authenticate( "LinkedIn" );
-		$user_profile = $linkedin->getUserProfile();
-		$exists = $db->linkedInLogin($user_profile->identifier);
-		if(!$exists)
+		if(!$linkedin->isUserConnected())
 		{
-			$query = 'INSERT INTO `users` SET `username`="' . mysql_real_escape_string($user_profile->displayName) . '",'
-					. '`password`="a",`fingerprint`=NULL,'
-					. '`email`="' . mysql_real_escape_string($user_profile->email) . '",'
-					. '`userlevel`="public",'
-					. '`realname`="' . mysql_real_escape_string($user_profile->firstName . ' ' . $user_profile->lastName) . '",'
-					. '`country`="' . mysql_real_escape_string($user_profile->country) . '",'
-					. '`linkedin_id`="' . mysql_real_escape_string($user_profile->identifier) . '",'
-					. '`linkedin_url`="' . mysql_real_escape_string($user_profile->profileURL) . '"';
-			$res = mysql_query($query);
-			if($res)
-			{
-				$db->linkedInLogin($user_profile->identifier);
-				echo("Larvol Sigma access granted.<br />");
-			}else{
-				echo("Sorry, there was a problem granting access based on LinkedIn authentication. Please try again at another time.<br />"
-						. mysql_error() . '<br />');
-			}
+			echo("LinkedIn authorization failed.<br />");
 		}else{
-			echo("Login successful.<br />");
+		
+			$user_profile = $linkedin->getUserProfile();
+			$linkedinlogin = $db->linkedInLogin($user_profile->email);
+			if(!$linkedinlogin)
+			{
+				$query = 'SELECT id FROM `users` WHERE`linkedin_id`="' . mysql_real_escape_string($user_profile->email) . '"';
+				$res = mysql_query($query);
+				if($res === false)
+				{
+					echo('There was a problem checking for the existence of your profile. Please try again at another time.<br />');
+				}else{
+					$res = mysql_fetch_assoc($res);
+					if($res !== false)
+					{
+						echo('There was a problem in the login system. Please try again at another time.<br />');
+					}else{
+						$query = 'INSERT INTO `users` SET `username`="' . mysql_real_escape_string($user_profile->displayName) . '",'
+								. '`password`="a",`fingerprint`=NULL,'
+								. '`email`="' . mysql_real_escape_string($user_profile->email) . '",'
+								. '`userlevel`="public",'
+								. '`realname`="' . mysql_real_escape_string($user_profile->firstName . ' ' . $user_profile->lastName) . '",'
+								. '`country`="' . mysql_real_escape_string($user_profile->country) . '",'
+								. '`linkedin_id`="' . mysql_real_escape_string($user_profile->email) . '",'
+								. '`linkedin_url`="' . mysql_real_escape_string($user_profile->profileURL) . '"';
+						$res = mysql_query($query);
+						if($res)
+						{
+							$db->linkedInLogin($user_profile->email);
+							echo("Larvol Sigma access granted.<br />");
+						}else{
+							echo("Sorry, there was a problem granting access based on LinkedIn authentication. Please try again at another time.<br />"
+									. mysql_error() . '<br />');
+						}
+					}
+				}
+			}else{
+				echo("Login successful.<br />");
+			}
 		}
 	}catch( Exception $e ){
 		echo "Social auth error: " . $e->getMessage();
