b6fdb2617ca52d7da6f07a4c71e799a48982167f
Saurabh Raj Singh <saurabh.singh@larvol.com>
2013-11-28 08:42:53 +0000

    [#61514800] Fixed, investigator name link in investigator tab of disease page goes to drilldown page but should go to investigator entity page TS: 45m

--- sigma/investigator_tracker.php
+++ sigma/investigator_tracker.php
@@ -1067,16 +1067,9 @@ function InvTrackerHTMLContent($data_matrix, $id, $rows, $columns, $investigator
 		$row = $incr;
 		
 		if($TrackerType != 'PTH')
-		$commonPart1 = 'ott.php?sourcepg=TZP&e1=' . $data_matrix[$row]['investigatorIds'];
+		$commonPart1 = $procommonPart1='investigator.php?InvestigatorId=' . $data_matrix[$row]['investigatorIds'].'&tab=Companies';
 		else
-		$commonPart1 = 'intermediary.php?e1=' . $data_matrix[$row]['investigatorIds'];
-		
-		
-		if($TrackerType != 'PTH')
-		$procommonPart1 = 'ott.php?sourcepg=TZP&e1=' . $data_matrix[$row]['investigatorIds'];
-		else
-		$procommonPart1 = 'intermediary.php?e1=' . $data_matrix[$row]['investigatorIds'];		
-		
+		$commonPart1 = $procommonPart1='intermediary.php?e1=' . $data_matrix[$row]['investigatorIds'];	
 		
 		$commonPart2 = '';
 		if($TrackerType == 'DIT') $commonPart2 = '&e2=' . $id;
