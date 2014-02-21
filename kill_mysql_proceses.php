ebbb358f0f9f6d200fd04f4d185762e70c2bfcd0
Tapaswi Panda <tapaswi.panda@larvol.com>
2014-02-21 05:38:27 +0000

    [#61239908], implemented mysqli for the script used for killing stalled mysql queries.

--- kill_mysql_proceses.php
+++ kill_mysql_proceses.php
@@ -1,14 +1,14 @@
 <?php  
 require_once('db.php');
 
-$connectSigmaUser = mysql_connect(DB_SERVER, DB_USER_SIGMA, DB_PASS_SIGMA) or die("Error connecting to database server!");
+$connectSigmaUser = mysqli_connect(DB_SERVER, DB_USER_SIGMA, DB_PASS_SIGMA) or die("Error connecting to database server!");
 
-$result = mysql_query("SHOW FULL PROCESSLIST", $connectSigmaUser);
+$result = mysqli_query($connectSigmaUser, "SHOW FULL PROCESSLIST");
 
 // writing all the long running query to log file for review purpose
 $processList  = "KILLED QUERY LOG ".date('m-d-Y H:i:s').PHP_EOL;
 
-while ($row = mysql_fetch_assoc($result)) {
+while ($row = mysqli_fetch_assoc($result)) {
 
 	//to log the quries for review purpose, we may comment it out later
 	//$processList  .= $row['Id'].'::'.$row['User']."::".$row['Time']."::".$row['Info'];
@@ -16,7 +16,7 @@ while ($row = mysql_fetch_assoc($result)) {
 	// if Query the query run from SIGMA and has been running since last 10 mins or more, we are killing the process.
 	if($row['Time'] >= 600) {
 		$process_id = $row['Id'];
-		if(mysql_query("KILL $process_id", $connectSigmaUser))
+		if(mysqli_query($connectSigmaUser, "KILL $process_id"))
 		$processList  .= $row['Id'].'::'.$row['User']."::".$row['Time']."::".$row['Info'];
 	}
 }
@@ -26,7 +26,7 @@ global $logger;
 $logger->warn($processList);
 
 //freeing the result of processlist
-mysql_free_result($result);
+mysqli_free_result($result);
 
 //closing the DB_USER_SIGMA's DB connection
-mysql_close($connectSigmaUser);
+mysqli_close($connectSigmaUser);
