bc40678c401aeca60578a735c8d263d650fff3d8
tkvinod <vinod.tk@larvol.com>
2014-03-31 18:08:18 +0530

    [#68211992]
    Added field LI_id to table tabs
    Some fixes in schema.sql and data.sql

--- setup/schema.sql
+++ setup/schema.sql
@@ -188,8 +188,10 @@ CREATE TABLE IF NOT EXISTS `tabs` (
   `investigators` int(11) NOT NULL,
   `news` int(11) NOT NULL,
   `trials` int(11) NOT NULL DEFAULT '0',
-  `last_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `last_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
 
 
 CREATE TABLE IF NOT EXISTS `settings` (
@@ -1234,6 +1236,7 @@ CREATE TABLE IF NOT EXISTS `redtags` (
  `rUIS` tinyint(1) NOT NULL DEFAULT '0',
   `formula`  	varchar(150) NULL,
   `statement`	varchar(500) NOT NULL,
+  `LI_id` VARCHAR(63) NULL DEFAULT NULL ,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
